67e9a93b7fc4d36e185d11a3067748fc8d7ac307
faaancychiiild <nino.mirziashvili7@gmail.com>
2022-07-26 21:53:39 +0400

    init: http server

---
+++ api/index.ts
@@ -0,0 +1,16 @@
+import express, { Application } from 'express';
+import dotenv from 'dotenv';
+import http from 'node:http';
+import logger from './utils/logger';
+
+dotenv.config();
+
+// create express listener function
+const app: Application = express();
+
+const PORT = process.env.PORT || 2700;
+
+const server = http.createServer(app);
+server.listen(PORT, () => {
+  logger.success(`Server is listening on port ${PORT}`);
+});
