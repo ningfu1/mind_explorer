00bccc4580f917a4781e8bba51002966b88515bf
faaancychiiild <nino.mirziashvili7@gmail.com>
2022-07-27 22:49:49 +0400

    ref: remove bg colors from logger

--- api/utils/logger.ts
+++ api/utils/logger.ts
@@ -1,8 +1,8 @@
 import chalk from 'chalk';
 
 export default {
-  info: (msg: string) => console.log(`${chalk.black.bgCyan(' INFO ')} [${new Date().toLocaleTimeString()}] ${msg}`),
+  info: (msg: string) => console.log(`${chalk.black.cyanBright(' INFO ')} [${new Date().toLocaleTimeString()}] ${msg}`),
   warn: (msg: string) => console.log(`${chalk.yellowBright('WARNING')} [${new Date().toLocaleTimeString()}] ${msg}`),
   debug: (msg: string) => console.log(`${chalk.red('DEBUG')} [${new Date().toLocaleTimeString()}] ${msg}`),
-  success: (msg: string) => console.log(`${chalk.black.bgGreenBright(' SUCCESS ')} [${new Date().toLocaleTimeString()}] ${msg}`),
+  success: (msg: string) => console.log(`${chalk.black.greenBright(' SUCCESS ')} [${new Date().toLocaleTimeString()}] ${msg}`),
 };
