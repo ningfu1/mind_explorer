83fef90b09ec7a6edad5b490fa021d6ab8baa9b2
faaancychiiild <nino.mirziashvili7@gmail.com>
2022-07-27 22:51:21 +0400

    logs:

--- api/providers/index.ts
+++ api/providers/index.ts
@@ -1,8 +1,10 @@
 import fs from 'fs';
 import { Step } from '../../types/main.types';
 import cache from '../cache';
+import logger from '../utils/logger';
 
 const setQuizData = () => {
+  logger.info('seeding database');
   const quiz_data: [Step] = JSON.parse(
     fs.readFileSync(`${__dirname}/data.json`, {
       encoding: 'utf-8',
@@ -14,6 +16,7 @@ const setQuizData = () => {
 
 /* generate random set of questions for a single user */
 const generate = (data: [Step]) => {
+  logger.info('generating random set of questions');
   data.forEach((item, index) => {
     let randomIndex = Math.floor(Math.random() * (data.length - 1));
     [data[index], data[randomIndex]] = [data[randomIndex], data[index]];
