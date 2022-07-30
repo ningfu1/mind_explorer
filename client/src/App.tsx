d57e0055a0454feb32fe6c821db89a1d84c28f86
faaancychiiild <nino.mirziashvili7@gmail.com>
2022-07-30 22:53:39 +0400

    routes: add routes to app

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,19 +1,22 @@
 import './App.css';
 import { ChakraProvider } from '@chakra-ui/react';
-import Question from './components/question';
 import { theme } from './utils/custom.chakra';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Quiz from './routes/quiz.route';
+import { useState } from 'react';
+import Landing from './routes/landing.route';
+import Result from './routes/result.route';
 
 function App() {
+  const [result, setResult] = useState('');
   return (
     <ChakraProvider theme={theme}>
-      <div
-        style={{
-          display: 'flex',
-          justifyContent: 'center',
-        }}
-      >
-        <Question value="How are you?"></Question>
-      </div>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Landing />} />
+          <Route path="/quiz" element={result ? <Result value={result} /> : <Quiz setResult={setResult} />} />
+        </Routes>
+      </Router>
     </ChakraProvider>
   );
 }
