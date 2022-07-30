ac4f0e58904e547ebd7473a89f05e0c807fc13e9
faaancychiiild <nino.mirziashvili7@gmail.com>
2022-07-30 22:29:20 +0400

    routes: add quiz page

---
+++ client/src/routes/quiz.route.tsx
@@ -0,0 +1,45 @@
+import Question from 'src/components/question';
+import Answer from 'src/components/answer';
+import { useEffect, useState } from 'react';
+import axios from '../utils/axios';
+import { API_URL } from 'src/utils/api.utils';
+import { sentTest } from '@types';
+
+const Quiz = (props: any) => {
+  const [step, setStep] = useState(0);
+  const [answer, setAnswer] = useState('');
+  const [question, setQuestion] = useState('');
+  const [choices, setChoices] = useState([]);
+
+  useEffect(() => {
+    axios.post(`${API_URL}/quiz/${step}`, { answer }).then((res) => {
+      const quiz: sentTest = res.data?.quiz;
+
+      if (res.data?.result) {
+        props.setResult(res.data.result);
+        return;
+      }
+
+      setQuestion(quiz?.test);
+      setChoices(quiz?.choices as never[]);
+    });
+  }, [step, answer, props]);
+
+  return (
+    <div
+      style={{
+        display: 'flex',
+        justifyContent: 'center',
+        flexDirection: 'column',
+        width: '80%',
+      }}
+    >
+      <Question value={question}></Question>
+      {choices?.map((choice) => (
+        <Answer key={choice} value={choice} step={step} setStep={setStep} setAnswer={setAnswer} />
+      ))}
+    </div>
+  );
+};
+
+export default Quiz;
