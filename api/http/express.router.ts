b019d082a0927408368366144abdadfdafc47e88
faaancychiiild <nino.mirziashvili7@gmail.com>
2022-07-31 11:49:42 +0400

    refactor: structure controllers

--- api/http/express.router.ts
+++ api/http/express.router.ts
@@ -1,10 +1,10 @@
 import { Router } from 'express';
-import controllers from './http.controllers';
+import * as controllers from './http.controllers';
 import middleware from './express.middleware';
 
 const router = Router();
 /* routes */
 router.get('/healthcheck', (req, res) => res.send('ok 200'));
-router.post('/quiz/:step', middleware.validate_sid, controllers.handle);
+router.post('/quiz/:step', middleware.validate_sid, controllers.quizHandler);
 
 export default router;
