ffb98a3f1de3a4ba1f5c7876a310b0906d3efb8b
faaancychiiild <nino.mirziashvili7@gmail.com>
2022-07-31 10:44:24 +0400

    test: setup client tests

--- client/src/App.test.tsx
+++ client/src/App.test.tsx
@@ -1,9 +1,22 @@
-import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from './App';
+import Quiz from './routes/quiz.route';
+import Result from './routes/result.route';
 
-test('renders learn react link', () => {
+test('runs the landing screen', async () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const startElement = screen.getByText(/Start Personality Test/i);
+  expect(startElement).toBeInTheDocument();
+});
+
+test('starts quiz', () => {
+  render(<Quiz />);
+  const step = screen.getByText(/Step/i);
+  expect(step.innerHTML).toBe('Step 0');
+});
+
+test('renders the result page', () => {
+  render(<Result value="introverted" />);
+  const val = screen.getByText(/introverted/i);
+  expect(val.innerHTML).toContain('introverted');
 });
